e510d7a3db5b98d01f4f77dd9dcccd7817145652
MarkAperstein <68637961+MarkAperstein@users.noreply.github.com>
2022-03-23 10:19:32 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,77 @@
 from network_functions import *
 from States import states, forbidden_transition_attempts, allowed_transition_attempts
 import json
+import pandas as pd
+from scipy.stats import chi2
+
 
 transition_matrices = {}
 # TODO: walk through the code, and try to understand what is happening.
 
 solver, shape = 'human', 'SPT'
+
 for size in ['Medium', 'Large']:
     if os.path.exists(size + '_transitions.txt'):
         with open(size + '_transitions.txt', 'r') as f:
+            "load list of experiments/paths"
             paths = json.load(f)
 
+    "all the possible states/nodes where proximity regions are  considered as states"
     state_order = sorted(states + forbidden_transition_attempts + allowed_transition_attempts)
+
+    "define network"
     my_network = Network(state_order, name='_'.join(['network', solver, size, shape]))
+
+    "adds paths from the json to the network path statistics, assigns weight to the edges based on the number of transition occurancies across all the paths"
     my_network.add_paths(paths)
-    transition_matrices[size] = my_network.transition_matrix().toarray()
-    my_network.plot_transition_matrix(title=size)
-    # TODO: in the transition matrix plots (in line 17), get rid of unoccupied states
+
+
+
+    """create higher order model. 
+    1. T: where each node represents a k length subpath
+    2. T_tag: based on the 1st order model which in general allows different transitions
+    """
+    hon=my_network.create_higher_order_network(k=2,null_model=False)
+    hon_null=my_network.create_higher_order_network(k=2,null_model=True)
+
+    "Transition matrices"
+    T=hon.transition_matrix().toarray()
+    T_tag=hon_null.transition_matrix().toarray()
+
+    "second largest eigenvalues"
+    ls=np.linalg.eigvals(T)
+    l_tags=np.linalg.eigvals(T_tag)
+
+    l=np.sort(np.abs(ls))[-2]
+    l_tag = np.sort(np.abs(l_tags))[-2]
+
+    "diffusion speed up/slow down. If>1 slow down. if <1 speedup"
+    S=np.log(l_tag)/np.log(l)
+    print(S)
+
+
+    """ 
+        Actually https://www.pathpy.net/tutorial/model_selection.html describes how to select the best higher order model.
+        Its not via the diffusion speedup/slow down but via statistical likhood. They already have the estimate of the order.
+        Seems that for both cases best order that describes the data is second order model.
+    """
+
+    p=my_network.paths
+    mog = pp.MultiOrderModel(p, max_order=10)
+    print('Optimal order = ', mog.estimate_order())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- network_functions.py
+++ network_functions.py
@@ -16,16 +16,20 @@ network_dir = os.path.join(os.getcwd(), 'Graphs')
 class Network(pp.Network):
     def __init__(self, possible_transitions: list, name='network'):
         super().__init__(directed=True)
+        "here you input all the states. Not transitions. Makes sense if you also have the option to stay in place"
         self.possible_transitions = possible_transitions
         self.name = name
+        "Paths object ment to hold path statistics"
         self.paths = pp.Paths()
         for state1, state2 in itertools.product(possible_transitions, possible_transitions):
+            "adding edges between all the posibble nodes including with itself"
             self.add_edge(state1, state2, weight=0)
 
     def add_paths(self, transitions: list) -> None:
         [self.paths.add_path(transition) for transition in transitions]
 
         for states, weight in self.paths.paths[1].items():
+            "for each transition between pair of states assign the weight based on statistics"
             if len(states) == 2:
                 self.add_edge(states[0], states[1], weight=weight[0])
 
@@ -92,49 +96,50 @@ class Network(pp.Network):
         plt.gcf().savefig(directory)
         plt.title(title)
 
-    # def create_higher_order_network(self, k: int = 2) -> pp.Network:
-    #     hon = pp.HigherOrderNetwork(self.paths, k=k, null_model=True)
-    #     # for e in hon.edges:
-    #     #     print(e, hon.edges[e])
-    #     return hon
-    #
-    # def absorbing_state_analysis(self) -> np.array:
-    #     T = self.transition_matrix().toarray()
-    #     m = transposeMatrix(T)
-    #     m = sort(m)
-    #     # norm = normalize(m)
-    #     trans = num_of_transients(m)
-    #     Q, R = decompose(m)
-    #     P = np.vstack([np.hstack([identity(1), np.zeros([1, trans])]),
-    #                    np.hstack([R, Q])])  # canonical form of transition matrix
-    #     N = np.linalg.inv(identity(len(Q[-1])) - Q)  # fundamental matrix
-    #     t = np.matmul(N, np.ones(N.shape[0]))  # expected number of steps before absorption from each steps
-    #     B = np.matmul(N, R)  # absorption probabilities
-    #     P_inf = np.linalg.matrix_power(P, 100)  # check whether P_inf is indeed np.array([[I, 0], [B, 0]])
-    #     return t
-    #
-    # def Markovian_analysis(self) -> np.array:
-    #     # adjacency_matrix, degrees, laplacian, transition matrix and eigenvector
-    #     A = self.adjacency_matrix().toarray()
-    #     D = np.eye(len(self.degrees())) * np.array(self.degrees())
-    #     L = self.laplacian_matrix(self.degrees()).toarray()
-    #     T = self.transition_matrix().toarray()
-    #     EV = self.leading_eigenvector(self.adjacency_matrix())
-    #     return T
-    #
-    # def higher_order_networks(self):
-    #     hon = self.create_higher_order_network()
-    #     hon.likelihood(self.paths)'
-    #     eigenvalue_gap = pp.algorithms.spectral.eigenvalue_gap(hon)
-    #
-    #     # How much slower is the second order network than the markovian network?
-    #     pp.algorithms.path_measures.slow_down_factor(self.paths)
-    #
-    #     # slow down factor
-    #     # Ratios smaller than one indicate that the temporal network exhibits non - Markovian characteristics
-    #     pp.algorithms.path_measures.entropy_growth_rate_ratio(self.paths, method='Miller')
-    #
-    #     # Is the process Markovian?
-    #     # estimate the order of the sequence (something like memory)
-    #     ms = pp.MarkovSequence(self.paths.sequence())
-    #     order = ms.estimate_order(4)
+    def create_higher_order_network(self, k = 2,null_model=True) -> pp.Network:
+        hon = pp.HigherOrderNetwork(self.paths, k=k, null_model=null_model)
+        # for e in hon.edges:
+        #     print(e, hon.edges[e])
+        return hon
+
+    def absorbing_state_analysis(self) -> np.array:
+        T = self.transition_matrix().toarray()
+        m = transiposeMatrix(T)
+        m = sort(m)
+        # norm = normalize(m)
+        trans = num_of_transients(m)
+        Q, R = decompose(m)
+        P = np.vstack([np.hstack([identity(1), np.zeros([1, trans])]),
+                       np.hstack([R, Q])])  # canonical form of transition matrix
+        N = np.linalg.inv(identity(len(Q[-1])) - Q)  # fundamental matrix
+        t = np.matmul(N, np.ones(N.shape[0]))  # expected number of steps before absorption from each steps
+        B = np.matmul(N, R)  # absorption probabilities
+        P_inf = np.linalg.matrix_power(P, 100)  # check whether P_inf is indeed np.array([[I, 0], [B, 0]])
+        return t
+
+    def Markovian_analysis(self) -> np.array:
+        # adjacency_matrix, degrees, laplacian, transition matrix and eigenvector
+        A = self.adjacency_matrix().toarray()
+        D = np.eye(len(self.degrees())) * np.array(self.degrees())
+        L = self.laplacian_matrix(self.degrees()).toarray()
+        T = self.transition_matrix().toarray()
+        EV = self.leading_eigenvector(self.adjacency_matrix())
+        return T
+
+    def higher_order_networks(self):
+        hon = self.create_higher_order_network()
+        hon.likelihood(self.paths)
+        eigenvalue_gap = pp.algorithms.spectral.eigenvalue_gap(hon)
+
+        # How much slower is the second order network than the markovian network?
+        pp.algorithms.path_measures.slow_down_factor(self.paths)
+
+        # slow down factor
+        # Ratios smaller than one indicate that the temporal network exhibits non - Markovian characteristics
+        pp.algorithms.path_measures.entropy_growth_rate_ratio(self.paths, method='Miller')
+
+        # Is the process Markovian?
+        # estimate the order of the sequence (something like memory)
+        ms = pp.MarkovSequence(self.paths.sequence())
+        order = ms.estimate_order(4)
+
